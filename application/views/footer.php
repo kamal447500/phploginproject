2df756af6608d27c3e7cfba2cb575bb2158627d2
kamal447500 <52102324+kamal447500@users.noreply.github.com>
2020-01-24 11:39:22 +0530

    Add files via upload

---
+++ application/views/footer.php
@@ -0,0 +1,4 @@
+<script type="text/js" src="?<php echo base_url('assets/js/bootstrap.bundle.js'); ?>"></script>
+<script type="text/js" src="?<php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Customer Website</title>
+	<link rel="stylesheet" href="?<php echo base_url('assets/css/bootstrap.css'); ?>">	
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Customer Details</a>
+		</div>
+	</div>
+</nav>
